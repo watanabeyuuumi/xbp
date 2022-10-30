4416df02e9bfc2fed7ca97de843cd8c67fd56e72
watanabeyuuumi <r202202064cq@jindai.jp>
2022-10-30 23:01:09 +0900

    2022103001

---
+++ de12/python/game.py
@@ -0,0 +1,50 @@
+name=input("名前を教えてください")
+month=int(input("あなたの誕生月は"))
+
+if month==1:
+    print(name,"さんは優しく温かい性格です")
+
+if month==2:
+    print(name,"さんは積極的で活発な性格です")   
+
+if month==3:
+    print(name,"さんは人当たりが良くて社交性抜群です")    
+
+if month==4:
+    print(name,"さんは自分に厳しく他人に優しい性格です")    
+
+if month==5:
+    print(name,"さんは情熱的で抜群の行動力がある人です")    
+
+if month==6:
+    print(name,"さんは強い好奇心と行動力がある人です")    
+
+if month==7:
+    print(name,"さんは思いやりがあって優しい人です")    
+
+if month==8:
+    print(name,"さんは華やかな性格で新しいものが好きな人です") 
+
+if month==9:
+    print(name,"さんは完全主義で繊細なこころを持っています")       
+
+if month==10:
+    print(name,"さんは責任感があってまじめです")    
+
+if month==11:
+    print(name,"マニアックな気質を持っています")    
+
+if month==12:
+    print(name,"さんは素直でマイペースです")    
+
+else:
+    print(name,"さん正しい月を記入してください")
+
+
+   
+
+    
+
+
+
+   
---
+++ de12/python/work3.py
@@ -0,0 +1,14 @@
+a=1
+if a > 10:
+    print("10より大きい")
+else:
+    print("10より小さい")
+
+
+
+
+name=input("名前を教えて下さい")
+waist=input("腹囲は？")
+age=input("年齢は？")
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
